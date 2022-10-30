439b1b8a84c021404fc4cb7a9fd38a1e63f2ccf6
juwel119 <juwelcse18@gmail.com>
2022-10-30 22:17:29 +0600

    Docker image create

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from .forms import CustomerForm
+
+
+
+def index(request):
+
+	form = CustomerForm()
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST)
+		if form.is_valid():
+			form.save()
+
+			
+	context = {'form':form}
+	return render(request, 'app/index.html', context)
+
+
